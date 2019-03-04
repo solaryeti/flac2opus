76fed74cb4fff9951da1c63542180b13981a0eda
Steven Meunier <code@solaryeti.com>
2019-03-04 19:33:30 +0000

    * src/Lib.hs: neaten

--- src/Lib.hs
+++ src/Lib.hs
@@ -36,7 +36,7 @@ data Opts = Opts
   { oWorkers :: Int
   , oVerbose :: Bool
   , oSrc     :: SrcFilePath
-  , oDest    :: DstFilePath
+  , oDst     :: DstFilePath
   }
 
 -- | Create a version of `System.FilePath.combine` that prepends the
@@ -48,75 +48,83 @@ joinPath parentPath file
   | otherwise = parentPath </> file
 
 -- | Find the flac files that are present under the `src`
--- directory but missing from the `dest` directory.
+-- directory but missing from the `dst` directory.
 filesToConvert :: SrcFilePath -> DstFilePath -> IO [FilePath]
-filesToConvert (SrcFilePath src) (DstFilePath dest) = do
+filesToConvert (SrcFilePath src) (DstFilePath dst) = do
     flacFiles <- F.find (pure True) (F.extension F.==? ".flac") src
-    filterM (missingOpusFile dest) flacFiles
+    filterM (missingOpusFile dst) flacFiles
   where
-    missingOpusFile dst file = not <$> doesFileExist (joinPath dst (replaceExtension file "opus"))
+    missingOpusFile :: FilePath -> FilePath -> IO Bool
+    missingOpusFile dst' file = not <$> doesFileExist (joinPath dst' (replaceExtension file "opus"))
 
--- | Convert a flac file file to opus, prepending the `dest` to the outfile.
+-- | Convert a flac file to opus, prepending the `dst` to the outfile.
 convertFile :: SrcFilePath -> DstFilePath -> IO ()
-convertFile (SrcFilePath file) (DstFilePath dest) = do
-  let outfile = replaceExtension (joinPath dest file ) "opus"
-  _ <- installHandler sigINT (Catch $ cleanup outfile) Nothing
-  createDirectoryIfMissing True (takeDirectory outfile)
-  (code, _, opuserr) <- procStrictWithErr "opusenc"
-                        [ "--bitrate"
-                        , "128"
-                        , "--vbr"
-                        , T.pack file
-                        , T.pack outfile ]
-                        empty
-  unless (code == ExitSuccess) $ do
-    -- Give some time for our cleanup operations in the
-    -- installHandler to do their thing
-    threadDelay 300000
-    putStrLn $ "Failed encoding: " ++ outfile
-    putStrLn ("opusenc output:" :: Text)
-    putStrLn opuserr
-    exitFailure
+convertFile (SrcFilePath src) (DstFilePath dst) = do
+    _ <- installHandler sigINT (Catch $ cleanup outfile) Nothing
+    createDirectoryIfMissing True (takeDirectory outfile)
+    (code, _, opuserr) <- procStrictWithErr "opusenc"
+                          [ "--bitrate"
+                          , "128"
+                          , "--vbr"
+                          , T.pack src
+                          , T.pack outfile ]
+                          empty
+    unless (code == ExitSuccess) $ fatalError opuserr
+  where
+    outfile :: FilePath
+    outfile = replaceExtension (joinPath dst src) "opus"
 
-cleanup :: FilePath -> IO ()
-cleanup file = do
-  fileExists <- doesFileExist file
-  when fileExists $ do
-    putStrLn $ "\nProcess failed. Cleaning up " <> T.pack file
-    removeFile file
+    fatalError :: Text -> IO ()
+    fatalError err = do
+      -- Give some time for our cleanup operations in the
+      -- installHandler to do their thing
+      threadDelay 300000
+      putStrLn $ "Failed encoding: " ++ outfile
+      putStrLn ("opusenc output:" :: Text)
+      putStrLn err
+      exitFailure
+
+    cleanup :: FilePath -> IO ()
+    cleanup file = do
+      fileExists <- doesFileExist file
+      when fileExists $ do
+        putStrLn $ "\nProcess failed. Cleaning up " <> T.pack file
+        removeFile file
 
 -- | Find the cover art files that are present under the `src`
--- directory but missing from the `dest` directory.
+-- directory but missing from the `dst` directory.
 coverArtFiles :: SrcFilePath -> DstFilePath -> IO [FilePath]
-coverArtFiles (SrcFilePath src) (DstFilePath dest) = do
+coverArtFiles (SrcFilePath src) (DstFilePath dst) = do
     artFiles <- F.find (pure True) (F.extension F.==? ".jpg" F.||? F.extension F.==? ".png") src
-    filterM (missingArtFile dest) artFiles
+    filterM (missingArtFile dst) artFiles
   where
-    missingArtFile dst file = not <$> doesFileExist (joinPath dst file)
+    missingArtFile :: FilePath -> FilePath -> IO Bool
+    missingArtFile dst' file = not <$> doesFileExist (joinPath dst' file)
 
 run :: Opts -> IO ()
-run (Opts workers verbose src dest) = do
+run (Opts workers verbose src dst) = do
     -- Encode flac files to opus
     doWithProgress filesToConvert convertFile "flac files"
+    putStrLn T.empty
     -- Copy cover art for opus files
     doWithProgress coverArtFiles copy "cover art"
 
  where
     doWithProgress :: (SrcFilePath -> DstFilePath -> IO [FilePath])  -- ^ Function to find files
-                   -> (SrcFilePath -> DstFilePath -> IO ())          -- ^ Action to apply to found files
+                   -> (SrcFilePath -> DstFilePath -> IO ())          -- ^ Function to apply to found files
                    -> Text                                           -- ^ Description of files to print in user messages
                    -> IO ()
     doWithProgress findFunc actionFunc description = do
       when verbose $ putStrLn $ "Finding " <> description <> "..."
-      files <- findFunc src dest
+      files <- findFunc src dst
       when verbose $ printFiles files
 
       displayConsoleRegions $ do
         pg <- newProgressBar def { pgWidth = 100
                                  , pgTotal = if not (null files) then toInteger $ length files else 1
-                                 , pgOnCompletion = Just "Done :percent after :elapsed seconds"
+                                 , pgOnCompletion = Just $ "Done :percent after :elapsed seconds (" ++ T.unpack  description ++ ")"
                                  }
-        mapPool_ workers (applyProgress pg actionFunc dest) (fmap SrcFilePath files)
+        mapPool_ workers (applyProgress pg actionFunc dst) (fmap SrcFilePath files)
         liftIO $ complete pg
 
     applyProgress :: ProgressBar
@@ -124,12 +132,16 @@ run (Opts workers verbose src dest) = do
                   -> DstFilePath
                   -> SrcFilePath
                   -> IO ()
-    applyProgress pg f dest' file = do
-      createDirectoryIfMissing True $ takeDirectory (joinPath (fromDstFilePath dest') (fromSrcFilePath file))
-      f file dest'
+    applyProgress pg f dst' src' = do
+      createDirectoryIfMissing True $ takeDirectory (joinPath (fromDstFilePath dst') (fromSrcFilePath src'))
+      f src' dst'
       barComplete <- isComplete pg
       unless barComplete $ tick pg
-    copy src' dst' = copyFile (fromSrcFilePath src') (joinPath (fromDstFilePath dst') (fromSrcFilePath src'))
+
+    copy :: SrcFilePath -> DstFilePath -> IO ()
+    copy (SrcFilePath src') (DstFilePath dst') = copyFile src' $ joinPath dst' src'
+
+    printFiles :: [FilePath] -> IO ()
     printFiles files = do
       putStrLn $ "Found " <> T.pack (show (length files)) <> " files:"
       mapM_ putStrLn files
@@ -137,8 +149,8 @@ run (Opts workers verbose src dest) = do
 -- | Create a pool of 'n' threads and map an IO function over a
 -- traversable using that pool to run the function concurrently.
 mapPool_ :: (Traversable t, NFData b)
-         => Int
-         -> (a -> IO b)
-         -> t a
+         => Int          -- ^ Pool size
+         -> (a -> IO b)  -- ^ Function to map
+         -> t a          -- ^ Traversable to map function over
          -> IO ()
 mapPool_ n f = Pool.runLimited n . traverse_ (Pool.fork . f)
