b8baff7afda995a9f35498e092c6418bb7ff8b9b
Steven Meunier <code@solaryeti.com>
2019-09-26 16:38:16 +0100

    Format with Brittany

--- app/Main.hs
+++ app/Main.hs
@@ -2,44 +2,54 @@
 
 module Main where
 
-import Lib
+import           Lib
 
-import Data.Version (showVersion)
-import Development.GitRev (gitHash)
-import Options.Applicative
-import Paths_flac2opus (version) -- Magic module that gets the version from the cabal file
-import Protolude hiding (option)
+import           Data.Version                   ( showVersion )
+import           Development.GitRev             ( gitHash
+                                                , gitCommitDate
+                                                )
+import           Options.Applicative
+import           Paths_flac2opus                ( version ) -- Magic module that gets the version from the cabal file
+import           Protolude               hiding ( option )
 
 opts :: ParserInfo Opts
-opts =
-    info (helper <*> versionOption <*> optsParser)
-           ( fullDesc
-          <> progDesc "Convert flac music files to opus."
-          <> header "flac2opus - music converter" )
+opts = info
+  (helper <*> versionOption <*> optsParser)
+  (fullDesc <> progDesc "Convert flac music files to opus." <> header
+    "flac2opus - music converter"
+  )
 
 optsParser :: Parser Opts
-optsParser = Opts
-      <$> option auto
-          ( long "workers"
-         <> short 'w'
-         <> help "Number of workers to spawn"
-         <> showDefault
-         <> value 1
-         <> metavar "INT" )
-      <*> switch
-          ( long "verbose"
-         <> short 'v'
-         <> help "Display verbose output" )
-      <*> (SrcFilePath <$> strArgument (metavar "SRC"))
-      <*> (DstFilePath <$> strArgument (metavar "DST"))
+optsParser =
+  Opts
+    <$> option
+          auto
+          (  long "workers"
+          <> short 'w'
+          <> help "Number of workers to spawn"
+          <> showDefault
+          <> value 1
+          <> metavar "INT"
+          )
+    <*> switch (long "verbose" <> short 'v' <> help "Display verbose output")
+    <*> (SrcFilePath <$> strArgument (metavar "SRC"))
+    <*> (DstFilePath <$> strArgument (metavar "DST"))
 
 -- | Version option that gets the version from the cabal file and the
 -- git hash.
 versionOption :: Parser (a -> a)
-versionOption =
-        infoOption
-            (concat ["flac2opus ", showVersion version, " ", $(gitHash)]) -- TemplateHaskell required for $(gitHash).
-            (long "version" <> help "Show version")
+versionOption = infoOption
+  (concat
+    [ "riff "
+    , showVersion version
+    , ", Git revision "
+    , $(gitHash)
+    , " ("
+    , $(gitCommitDate)
+    , ")"
+    ]
+  ) -- TemplateHaskell required for gitrev functions.
+  (long "version" <> help "Show version")
 
 main :: IO ()
 main = execParser opts >>= run
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,33 +1,49 @@
 {-|
 Module      : Lib
-Description : Lib's main module
+Description : Main library for flac2opus
 
-This is a haddock comment describing your library
-For more information on how to write Haddock comments check the user guide:
-<https://www.haskell.org/haddock/doc/html/index.html>
 -}
 
 module Lib
-    ( Opts(..)
-    , SrcFilePath(..)
-    , DstFilePath(..)
-    , run
-    ) where
+  ( Opts(..)
+  , SrcFilePath(..)
+  , DstFilePath(..)
+  , run
+  )
+where
 
 import           Lib.Prelude
 
-import           Control.Concurrent (threadDelay)
-import           Control.Monad (filterM, when)
+import           Control.Concurrent             ( threadDelay )
+import           Control.Monad                  ( filterM
+                                                , when
+                                                )
 import           System.Console.AsciiProgress
-import           System.Directory (createDirectoryIfMissing, removeFile, doesFileExist, copyFile)
-import           System.Exit (ExitCode(..), exitFailure)
-import           System.FilePath (FilePath, replaceExtension, takeDirectory, isAbsolute, dropDrive, (</>))
-import           System.Posix.Signals (installHandler, Handler(..), sigINT)
-import           Turtle (procStrictWithErr)
+import           System.Directory               ( createDirectoryIfMissing
+                                                , removeFile
+                                                , doesFileExist
+                                                , copyFile
+                                                )
+import           System.Exit                    ( ExitCode(..)
+                                                , exitFailure
+                                                )
+import           System.FilePath                ( FilePath
+                                                , replaceExtension
+                                                , takeDirectory
+                                                , isAbsolute
+                                                , dropDrive
+                                                , (</>)
+                                                )
+import           System.Posix.Signals           ( installHandler
+                                                , Handler(..)
+                                                , sigINT
+                                                )
+import           Turtle                         ( procStrictWithErr )
 
-import qualified Data.Text as T
-import qualified Control.Concurrent.PooledIO.Final as Pool
-import qualified System.FilePath.Find as F
+import qualified Data.Text                     as T
+import qualified Control.Concurrent.PooledIO.Final
+                                               as Pool
+import qualified System.FilePath.Find          as F
 
 newtype SrcFilePath = SrcFilePath { fromSrcFilePath :: FilePath }
 newtype DstFilePath = DstFilePath { fromDstFilePath :: FilePath }
@@ -43,114 +59,124 @@ data Opts = Opts
 -- given `parentPath` even when the `file` is absolute rather than
 -- just returning the `file` unchanged.
 joinPath :: FilePath -> FilePath -> FilePath
-joinPath parentPath file
-  | isAbsolute file = parentPath </> dropDrive file
-  | otherwise = parentPath </> file
+joinPath parentPath file | isAbsolute file = parentPath </> dropDrive file
+                         | otherwise       = parentPath </> file
 
 -- | Find the flac files that are present under the `src`
 -- directory but missing from the `dst` directory.
 filesToConvert :: SrcFilePath -> DstFilePath -> IO [FilePath]
 filesToConvert (SrcFilePath src) (DstFilePath dst) = do
-    flacFiles <- F.find (pure True) (F.extension F.==? ".flac") src
-    filterM (missingOpusFile dst) flacFiles
-  where
-    missingOpusFile :: FilePath -> FilePath -> IO Bool
-    missingOpusFile dst' file = not <$> doesFileExist (joinPath dst' (replaceExtension file "opus"))
+  flacFiles <- F.find (pure True) (F.extension F.==? ".flac") src
+  filterM (missingOpusFile dst) flacFiles
+ where
+  missingOpusFile :: FilePath -> FilePath -> IO Bool
+  missingOpusFile dst' file =
+    not <$> doesFileExist (joinPath dst' (replaceExtension file "opus"))
 
 -- | Convert a flac file to opus, prepending the `dst` to the outfile.
 convertFile :: SrcFilePath -> DstFilePath -> IO ()
 convertFile (SrcFilePath src) (DstFilePath dst) = do
-    _ <- installHandler sigINT (Catch $ cleanup outfile) Nothing
-    createDirectoryIfMissing True (takeDirectory outfile)
-    (code, _, opuserr) <- procStrictWithErr "opusenc"
-                          [ "--bitrate"
-                          , "128"
-                          , "--vbr"
-                          , T.pack src
-                          , T.pack outfile ]
-                          empty
-    unless (code == ExitSuccess) $ fatalError opuserr
-  where
-    outfile :: FilePath
-    outfile = replaceExtension (joinPath dst src) "opus"
+  _ <- installHandler sigINT (Catch $ cleanup outfile) Nothing
+  createDirectoryIfMissing True (takeDirectory outfile)
+  (code, _, opuserr) <- procStrictWithErr
+    "opusenc"
+    ["--bitrate", "128", "--vbr", T.pack src, T.pack outfile]
+    empty
+  unless (code == ExitSuccess) $ fatalError opuserr
+ where
+  outfile :: FilePath
+  outfile = replaceExtension (joinPath dst src) "opus"
 
-    fatalError :: Text -> IO ()
-    fatalError err = do
-      -- Give some time for our cleanup operations in the
-      -- installHandler to do their thing
-      threadDelay 300000
-      putStrLn $ "Failed encoding: " ++ outfile
-      putStrLn ("opusenc output:" :: Text)
-      putStrLn err
-      exitFailure
+  fatalError :: Text -> IO ()
+  fatalError err = do
+    -- Give some time for our cleanup operations in the
+    -- installHandler to do their thing
+    threadDelay 300000
+    putStrLn $ "Failed encoding: " ++ outfile
+    putStrLn ("opusenc output:" :: Text)
+    putStrLn err
+    exitFailure
 
-    cleanup :: FilePath -> IO ()
-    cleanup file = do
-      fileExists <- doesFileExist file
-      when fileExists $ do
-        putStrLn $ "\nProcess failed. Cleaning up " <> T.pack file
-        removeFile file
+  cleanup :: FilePath -> IO ()
+  cleanup file = do
+    fileExists <- doesFileExist file
+    when fileExists $ do
+      putStrLn $ "\nProcess failed. Cleaning up " <> T.pack file
+      removeFile file
 
 -- | Find the cover art files that are present under the `src`
 -- directory but missing from the `dst` directory.
 coverArtFiles :: SrcFilePath -> DstFilePath -> IO [FilePath]
 coverArtFiles (SrcFilePath src) (DstFilePath dst) = do
-    artFiles <- F.find (pure True) (F.extension F.==? ".jpg" F.||? F.extension F.==? ".png") src
-    filterM (missingArtFile dst) artFiles
-  where
-    missingArtFile :: FilePath -> FilePath -> IO Bool
-    missingArtFile dst' file = not <$> doesFileExist (joinPath dst' file)
+  artFiles <- F.find (pure True)
+                     (F.extension F.==? ".jpg" F.||? F.extension F.==? ".png")
+                     src
+  filterM (missingArtFile dst) artFiles
+ where
+  missingArtFile :: FilePath -> FilePath -> IO Bool
+  missingArtFile dst' file = not <$> doesFileExist (joinPath dst' file)
 
 run :: Opts -> IO ()
 run (Opts workers verbose src dst) = do
     -- Encode flac files to opus
-    doWithProgress filesToConvert convertFile "flac files"
-    putStrLn T.empty
-    -- Copy cover art for opus files
-    doWithProgress coverArtFiles copy "cover art"
+  doWithProgress filesToConvert convertFile "flac files"
+  putStrLn T.empty
+  -- Copy cover art for opus files
+  doWithProgress coverArtFiles copy "cover art"
 
  where
-    doWithProgress :: (SrcFilePath -> DstFilePath -> IO [FilePath])  -- ^ Function to find files
-                   -> (SrcFilePath -> DstFilePath -> IO ())          -- ^ Function to apply to found files
-                   -> Text                                           -- ^ Description of files to print in user messages
-                   -> IO ()
-    doWithProgress findFunc actionFunc description = do
-      when verbose $ putStrLn $ "Finding " <> description <> "..."
-      files <- findFunc src dst
-      when verbose $ printFiles files
+  doWithProgress
+    :: (SrcFilePath -> DstFilePath -> IO [FilePath])  -- ^ Function to find files
+    -> (SrcFilePath -> DstFilePath -> IO ())          -- ^ Function to apply to found files
+    -> Text                                           -- ^ Description of files to print in user messages
+    -> IO ()
+  doWithProgress findFunc actionFunc description = do
+    when verbose $ putStrLn $ "Finding " <> description <> "..."
+    files <- findFunc src dst
+    when verbose $ printFiles files
 
-      displayConsoleRegions $ do
-        pg <- newProgressBar def { pgWidth = 100
-                                 , pgTotal = if not (null files) then toInteger $ length files else 1
-                                 , pgOnCompletion = Just $ "Done :percent after :elapsed seconds (" ++ T.unpack  description ++ ")"
-                                 }
-        mapPool_ workers (applyProgress pg actionFunc dst) (fmap SrcFilePath files)
-        liftIO $ complete pg
+    displayConsoleRegions $ do
+      pg <- newProgressBar def
+        { pgWidth = 100
+        , pgTotal = if not (null files) then toInteger $ length files else 1
+        , pgOnCompletion = Just
+                           $  "Done :percent after :elapsed seconds ("
+                           ++ T.unpack description
+                           ++ ")"
+        }
+      mapPool_ workers
+               (applyProgress pg actionFunc dst)
+               (fmap SrcFilePath files)
+      liftIO $ complete pg
 
-    applyProgress :: ProgressBar
-                  -> (SrcFilePath -> DstFilePath -> IO ())
-                  -> DstFilePath
-                  -> SrcFilePath
-                  -> IO ()
-    applyProgress pg f dst' src' = do
-      createDirectoryIfMissing True $ takeDirectory (joinPath (fromDstFilePath dst') (fromSrcFilePath src'))
-      f src' dst'
-      barComplete <- isComplete pg
-      unless barComplete $ tick pg
+  applyProgress
+    :: ProgressBar
+    -> (SrcFilePath -> DstFilePath -> IO ())
+    -> DstFilePath
+    -> SrcFilePath
+    -> IO ()
+  applyProgress pg f dst' src' = do
+    createDirectoryIfMissing True $ takeDirectory
+      (joinPath (fromDstFilePath dst') (fromSrcFilePath src'))
+    f src' dst'
+    barComplete <- isComplete pg
+    unless barComplete $ tick pg
 
-    copy :: SrcFilePath -> DstFilePath -> IO ()
-    copy (SrcFilePath src') (DstFilePath dst') = copyFile src' $ joinPath dst' src'
+  copy :: SrcFilePath -> DstFilePath -> IO ()
+  copy (SrcFilePath src') (DstFilePath dst') =
+    copyFile src' $ joinPath dst' src'
 
-    printFiles :: [FilePath] -> IO ()
-    printFiles files = do
-      putStrLn $ "Found " <> T.pack (show (length files)) <> " files:"
-      mapM_ putStrLn files
+  printFiles :: [FilePath] -> IO ()
+  printFiles files = do
+    putStrLn $ "Found " <> T.pack (show (length files)) <> " files:"
+    mapM_ putStrLn files
 
 -- | Create a pool of 'n' threads and map an IO function over a
 -- traversable using that pool to run the function concurrently.
-mapPool_ :: (Traversable t, NFData b)
-         => Int          -- ^ Pool size
-         -> (a -> IO b)  -- ^ Function to map
-         -> t a          -- ^ Traversable to map function over
-         -> IO ()
+mapPool_
+  :: (Traversable t, NFData b)
+  => Int          -- ^ Pool size
+  -> (a -> IO b)  -- ^ Function to map
+  -> t a          -- ^ Traversable to map function over
+  -> IO ()
 mapPool_ n f = Pool.runLimited n . traverse_ (Pool.fork . f)
